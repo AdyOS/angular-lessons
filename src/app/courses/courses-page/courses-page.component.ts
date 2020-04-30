278e1551968f0f06d6ab9c74ef9c7e2d3aa90ad1
andrey.osipov <andrei_osipov1@epam.com>
2020-04-30 10:53:30 +0300

    Complete task 2 - add event handlers - and input bindings - add @output declarations

--- src/app/courses/courses-page/courses-page.component.ts
+++ src/app/courses/courses-page/courses-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-courses-page',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoursesPageComponent implements OnInit {
 
+  @Input()
+  public searchValue: string | number;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.searchValue = '';
   }
 
+  onSearchClick(): void {
+    console.log('search value:', this.searchValue);
+  }
 }
